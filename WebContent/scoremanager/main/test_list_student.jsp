f9a583055adf7cfb705589d4d5c8662f1a1b4bf5
shinsho <shinsho@LAPTOP-9HVLMJ1S>
2024-05-07 09:24:58 +0900

    成績参照（生徒検索）

---
+++ WebContent/scoremanager/main/test_list_student.jsp
@@ -0,0 +1,40 @@
+<%-- 学生一覧JSP --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/common/base.jsp" >
+	<c:param name="title">
+		得点管理システム
+	</c:param>
+
+	<c:param name="scripts"></c:param>
+
+	<c:param name="content">
+		<section class="me-4">
+		<h2 class="h3 mb-3 fw-norma bg-secondary bg-opacity py-2 px-4">成績一覧（学生）</h2>
+		<div class="my-2 text-end px-4">
+			<a href="SubjectCreate.action">新規登録</a>
+		</div>
+
+
+				<table class="table table-hover" >
+					<tr>
+						<th>科目名</th>
+						<th>科目コード</th>
+						<th>回数</th>
+						<th>点数</th>
+					</tr>
+					<c:forEach var="subject" items="${subjects}">
+						<tr>
+							<td>${subject.name}</td>
+							<td>${subject.cd}</td>
+							<td>${subject.cd}"</td>
+							<td>${subject.cd}"</td>
+						</tr>
+					</c:forEach>
+				</table>
+
+
+		</section>
+	</c:param>
+</c:import>
---
+++ src/scoremanager/main/TestListStudentExecuteAction.java
@@ -0,0 +1,42 @@
+package scoremanager.main;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import bean.Test;
+import dao.TestDao;
+import tool.Action;
+
+public class TestListStudentExecuteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		HttpSession session = request.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		String studentNo = request.getParameter("student_no");
+
+        if (studentNo != null && !studentNo.isEmpty()) {
+
+            try {
+                TestDao testDao = new TestDao(); // TestDaoのインスタンスを作成
+
+                // 学生のテストを取得
+                List<Test> studentTests = testDao.filter(studentNo); // 学生番号を使って学生のテストを取得するメソッドを仮定
+
+                // リクエスト属性に学生のテストを設定してJSPに転送
+                request.setAttribute("testList", studentTests);
+                request.getRequestDispatcher("test_list_student.jsp").forward(request, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            // 学生番号が提供されていない場合の処理
+        }
+	}
+}
