3f5f6e6b889b53c4f53d55dd6f815bf5cab3ebca
SS0622 <tky2302009@stu.o-hara.ac.jp>
2024-05-15 10:03:47 +0900

    修正var2.1

--- WebContent/scoremanager/main/test_regist.jsp
+++ WebContent/scoremanager/main/test_regist.jsp
@@ -2,7 +2,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.List" %>
 <c:import url="/common/base.jsp" >
+
 	<c:param name="title">
 		得点管理システム
 	</c:param>
@@ -16,17 +19,17 @@
 
 		<form action="TestRegistExecute.action" method="post">
 			<div class="col-4">
-				<table>
+				<table style="border: 2px solid black; border-collapse: separate; border-radius: 20px; overflow: hidden; width: 400%;">
 					<tr>
-						<th>入学年度</th>
-						<th>クラス</th>
-						<th>科目</th>
-						<th>回数</th>
+						<th style="padding: 8px;">入学年度</th>
+						<th style="padding: 8px;">クラス</th>
+						<th style="padding: 8px;">科目</th>
+						<th style="padding: 8px;">回数</th>
 					</tr>
 					<tr>
 						<td>
 							<!-- 入学年度 -->
-							<select class="form-select " id="select" name="f1">
+							<select class="form-select " id="select" name="ent_year">
 								<option value="${ent_year}">
 									<c:choose>
 			    						<c:when test="${empty ent_year}">
@@ -38,13 +41,13 @@
 									</c:choose>
 								</option>
 								<c:forEach var="ent_year" items="${ent_year_set}">
-									<option value="${ent_year}">${ent_year}</option>
+									<option value="${ent_year}" style="padding: 8px;">${ent_year}</option>
 								</c:forEach>
 							</select>
 						</td>
 						<td>
 							<!-- クラス番号 -->
-							<select class="form-select " id="select" name="f2">
+							<select class="form-select " id="select" name="class_num">
 								<option value="${class_num}">
 									<c:choose>
 			    						<c:when test="${empty class_num}">
@@ -56,31 +59,32 @@
 									</c:choose>
 								</option>
 								<c:forEach var="class_num" items="${class_num_set}">
-									<option value="${class_num}">${class_num}</option>
+									<option value="${class_num}" style="padding: 8px;">${class_num}</option>
 								</c:forEach>
 							</select>
 						</td>
 						<td>
 							<!-- 科目名 -->
-							<select class="form-select " id="select" name="f3">
-								<option value="${subject}">
+							<select class="form-select " id="select" name="subject_name">
+								<option value="${subject.name}">
 									<c:choose>
 			    						<c:when test="${empty subject}">
 											--------
 			    						</c:when>
 			    						<c:otherwise>
-											${subject }
+											${subject.name }
 			    						</c:otherwise>
 									</c:choose>
 								</option>
 								<c:forEach var="subject" items="${subject_set}">
-									<option value="${subject.name}">${subject.name}</option>
+									<option value="${subject.name}" style="padding: 8px;">${subject.name}</option>
+									<input type="hidden" name="subject_cd" value="${subject.cd }">
 								</c:forEach>
 							</select>
 						</td>
 						<td>
 							<!-- 回数 -->
-							<select class="form-select " id="select" name="f4">
+							<select class="form-select " id="select" name="no">
 								<option value="${count}">
 									<c:choose>
 			    						<c:when test="${empty count}">
@@ -92,7 +96,7 @@
 									</c:choose>
 								</option>
 								<c:forEach var="count" items="${no_set}">
-									<option value="${count}">${count}</option>
+									<option value="${count}" style="padding: 8px;">${count}</option>
 								</c:forEach>
 							</select>
 						</td>
@@ -101,43 +105,55 @@
 				<c:if test="${error1}">
             		<p style="color: yellow;">入学年度とクラスと科目と回数を選択して下さい</p>
         		</c:if>
+        		<input type="hidden" name="flag" value="t">
 
         		<button type="submit">検索</button>
 
 			</div>
 		</form>
+
 		<c:if test="${done}">
-			<form action="TestRegistExcute.action" method="post">
-				<div class="col-4">
-					<h2>科目："${subject }"（"${count }"回）</h2>
-					<table>
-						<tr>
-							<th>入学年度</th>
-							<th>クラス</th>
-							<th>学生番号</th>
-							<th>氏名</th>
-							<th>点数</th>
-						</tr>
-						<c:forEach var="test" items="${testSet}">
-							<tr>
-								<td>"${test.ent_year }"	<input type="hidden" name="ent_year" value="${test.ent_year }"></td>
-								<td>"${test.class }" <input type="hidden" name="class_num" value="${test.class }"></td>
-								<td>"${test.no }" <input type="hidden" name="no" value="${test.no }"></td>
-								<td>"${test.name }" <input type="hidden" name="name" value="${test.name }"></td>
-								<td>
-								<input type="text" name="point_${no }" value="${point }">
-								<div>
-									<c:if test="${error2}">
-		            					<p style="color: yellow;">0～100の範囲で入力してください</p>
-		        					</c:if>
-								</div>
-								</td>
-							</tr>
-						</c:forEach>
-					</table>
-					<button type="submit">登録して終了</button>
-				</div>
-			</form>
+			<c:choose>
+			    <c:when test="${test_set.size()>0}">
+					<form action="TestRegistExecute.action" method="post">
+					    <%-- リストを作成 --%>
+		    			<%
+		        			List<String> dataList = new ArrayList<>();
+		    			%>
+						<div class="col-4">
+							<h2>科目："${subject_name}"（"${no_now}"回）</h2>
+							<table>
+								<tr>
+									<th>入学年度</th>
+									<th>クラス</th>
+									<th>学生番号</th>
+									<th>氏名</th>
+									<th>点数</th>
+								</tr>
+								<c:forEach var="test" items="${test_set}">
+									<tr>
+										<td>"${test.student.entYear }"	<input type="hidden" name="ent_year" value="${test.student.entYear}"></td>
+										<td>"${test.student.classNum }" <input type="hidden" name="class_num" value="${test.student.classNum }"></td>
+										<td>"${test.student.no}" <input type="hidden" name="no" value="${test.student.no}"></td>
+										<td>"${test.student.name}" <input type="hidden" name="name" value="${test.student.name}"></td>
+										<td>"${test.point}" <input type="text" name="point" value="${point}">
+										<div>
+											<c:if test="${error2}">
+				            					<p style="color: yellow;">0～100の範囲で入力してください</p>
+				        					</c:if>
+										</div>
+										</td>
+									</tr>
+								</c:forEach>
+							</table>
+							<button type="submit">登録して終了</button>
+						</div>
+					</form>
+				</c:when>
+				<c:otherwise>
+					<p>存在しません</p>
+				</c:otherwise>
+			</c:choose>
 		</c:if>
 
 		<div class="my-2 text-end px-4">
--- src/scoremanager/main/TestRegistExecuteAction.java
+++ src/scoremanager/main/TestRegistExecuteAction.java
@@ -30,112 +30,121 @@ public class TestRegistExecuteAction extends Action {
 		HttpSession session = request.getSession();//セッション
 		Teacher teacher = (Teacher)session.getAttribute("user");
 
-		StudentDao stuDao = new StudentDao();//クラス番号Daoを初期化
-		SubjectDao subDao = new SubjectDao();//科目Dao
-		TestDao tesDao = new TestDao();//科目Dao
+		String flag=request.getParameter("flag");
+		if(flag!=null){
+			System.out.println("aa");
+			StudentDao stuDao = new StudentDao();//クラス番号Daoを初期化
+			SubjectDao subDao = new SubjectDao();//科目Dao
+			TestDao tesDao = new TestDao();//科目Dao
 
-		// test_regist.jspからデータを受け取る
-		String entYearStr=request.getParameter("ent_year");
-		String classNum=request.getParameter("class_num");
-		String subjectCd=request.getParameter("subject_cd");
-		String subjectName=request.getParameter("subject_name");
-		String noStr=request.getParameter("no");
+			// test_regist.jspからデータを受け取る
+			String entYearStr=request.getParameter("ent_year");
+			String classNum=request.getParameter("class_num");
+			String subjectCd=request.getParameter("subject_cd");
+			String subjectName=request.getParameter("subject_name");
+			String noStr=request.getParameter("no");
 
-    	School school = new School();
-    	school = teacher.getSchool();
-    	Subject subject = new Subject();
-    	subject.setCd(subjectCd);
-    	subject.setName(subjectName);
-    	subject.setSchool(school);
+	    	School school = new School();
+	    	school = teacher.getSchool();
+	    	Subject subject = new Subject();
+	    	subject.setCd(subjectCd);
+	    	subject.setName(subjectName);
+	    	subject.setSchool(school);
+	    	System.out.println(subjectCd);
 
-		LocalDate todaysDate = LocalDate.now();//LocalDateインスタンスを取得
-		int year = todaysDate.getYear();//現在の年を取得
-		ClassNumDao cNumDao = new ClassNumDao();//クラス番号Daoを初期化
-		SubjectCdDao subCdDao = new SubjectCdDao();//科目Dao
-		TestNoDao testNoDao = new TestNoDao();
+			LocalDate todaysDate = LocalDate.now();//LocalDateインスタンスを取得
+			int year = todaysDate.getYear();//現在の年を取得
+			ClassNumDao cNumDao = new ClassNumDao();//クラス番号Daoを初期化
+			SubjectCdDao subCdDao = new SubjectCdDao();//科目Dao
+			TestNoDao testNoDao = new TestNoDao();
 
-		boolean done = false;
-		//入学年度リスト
-		// リストを初期化
-		List<Integer> entYearSet = new ArrayList<>();
-		// 10年前から1年後まで年をリストに追加
-		for (int i= year - 10; i < year + 1; i++) {
-			entYearSet.add(i);
+			boolean done = false;
+			//入学年度リスト
+			// リストを初期化
+			List<Integer> entYearSet = new ArrayList<>();
+			// 10年前から1年後まで年をリストに追加
+			for (int i= year - 10; i < year + 1; i++) {
+				entYearSet.add(i);
+			}
+
+			//回数リスト
+			// リストを初期化
+			List<Integer> noSet = new ArrayList<>();
+			// 1～9までをリストに追加
+
+
+			//DBからデータ取得 3
+			// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
+			List<String> cNumList = cNumDao.filter(teacher.getSchool());
+			// ログインユーザーの学校コードをもとに科目一覧を取得
+			List<Subject> subList = subCdDao.filter(teacher.getSchool());
+	//		//科目名リスト
+			noSet=testNoDao.filter(teacher.getSchool());
+	//		List<String> subName = new ArrayList<>();
+	//		for(int i = 0; i < subList.size(); i++){
+	//			subName.add(subList.get(i).getName());
+	//		}
+
+			//レスポンス値をセット 6
+			// リクエストに入学年度をセット
+			request.setAttribute("ent_year_set", entYearSet);
+			// リクエストにクラス番号をセット
+			request.setAttribute("class_num_set", cNumList);
+			// リクエストに科目リストをセット
+			request.setAttribute("subject_set", subList);
+			// リクエストに回数をセット
+			request.setAttribute("no_set", noSet);
+
+	        request.setAttribute("ent_year", entYearStr);
+	        request.setAttribute("class_num", classNum);
+	        request.setAttribute("subject", subject);
+	    	request.setAttribute("no", noStr);
+
+	        // バリデーションチェック
+			boolean[] errors ={false, false, false, false};
+			if(entYearStr == null || entYearStr.isEmpty()){
+				errors[0]=true;
+			}
+	        if (classNum == null || classNum.isEmpty()) {
+	        	errors[1]=true;
+	        }
+	        if (subjectName == null || subjectName.isEmpty()) {
+	        	errors[2]=true;
+	        }
+	        if (noStr == null || noStr.isEmpty()) {
+	        	errors[3]=true;
+	        }
+	        if (errors[0] || errors[1] || errors[2] || errors[3]) {
+
+	            // 入力されたデータとエラーメッセージをリクエストにセット
+	        	request.setAttribute("done", done);
+
+	            // 入力画面にフォワード
+	            RequestDispatcher dispatcher = request.getRequestDispatcher("test_regist.jsp");
+	            dispatcher.forward(request, response);
+	        }else{
+	        	done=true;
+	        	System.out.println("bb");
+	        	done=true;
+	        	int entYear = Integer.parseInt(entYearStr);
+	        	int no = Integer.parseInt(noStr);
+
+
+	        	// 成績インスタンスを初期化
+	        	TestDao tDao = new TestDao();
+	        	// 成績インスタンスに検索結果をセット
+	        	List<Test>testSet=tDao.filter(entYear, classNum, subject, no, school);
+
+	    		// リクエストに検索したかのチェックをセット
+	    		request.setAttribute("done", done);
+
+	        	request.setAttribute("test_set", testSet);
+
+	        	request.getRequestDispatcher("test_regist.jsp").forward(request, response);
+	        }
+		}else{
+			request.getRequestDispatcher("test_regist_done.jsp").forward(request, response);
 		}
-
-		//回数リスト
-		// リストを初期化
-		List<Integer> noSet = new ArrayList<>();
-		// 1～9までをリストに追加
-
-
-		//DBからデータ取得 3
-		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-		List<String> cNumList = cNumDao.filter(teacher.getSchool());
-		// ログインユーザーの学校コードをもとに科目一覧を取得
-		List<Subject> subList = subCdDao.filter(teacher.getSchool());
-//		//科目名リスト
-		noSet=testNoDao.filter(teacher.getSchool());
-//		List<String> subName = new ArrayList<>();
-//		for(int i = 0; i < subList.size(); i++){
-//			subName.add(subList.get(i).getName());
-//		}
-
-		//レスポンス値をセット 6
-		// リクエストに入学年度をセット
-		request.setAttribute("ent_year_set", entYearSet);
-		// リクエストにクラス番号をセット
-		request.setAttribute("class_num_set", cNumList);
-		// リクエストに科目リストをセット
-		request.setAttribute("subject_set", subList);
-		// リクエストに回数をセット
-		request.setAttribute("no_set", noSet);
-        request.setAttribute("ent_year", entYearStr);
-        request.setAttribute("class_num", classNum);
-        request.setAttribute("subject", subject);
-    	request.setAttribute("no", noStr);
-
-        // バリデーションチェック
-		boolean[] errors ={false, false, false, false};
-		if(entYearStr == null || entYearStr.isEmpty()){
-			errors[0]=true;
-		}
-        if (classNum == null || classNum.isEmpty()) {
-        	errors[1]=true;
-        }
-        if (subjectName == null || subjectName.isEmpty()) {
-        	errors[2]=true;
-        }
-        if (noStr == null || noStr.isEmpty()) {
-        	errors[3]=true;
-        }
-        if (errors[0] || errors[1] || errors[2] || errors[3]) {
-
-            // 入力されたデータとエラーメッセージをリクエストにセット
-        	request.setAttribute("done", done);
-
-            // 入力画面にフォワード
-            RequestDispatcher dispatcher = request.getRequestDispatcher("test_regist.jsp");
-            dispatcher.forward(request, response);
-        }else{
-
-        	done=true;
-        	int entYear = Integer.parseInt(entYearStr);
-        	int no = Integer.parseInt(noStr);
-
-
-        	// 成績インスタンスを初期化
-        	TestDao tDao = new TestDao();
-        	// 成績インスタンスに検索結果をセット
-        	List<Test>testSet=tDao.filter(entYear, classNum, subject, no, school);
-
-    		// リクエストに検索したかのチェックをセット
-    		request.setAttribute("done", done);
-
-        	request.setAttribute("test_set", testSet);
-
-        	request.getRequestDispatcher("test_regist.jsp").forward(request, response);
-        }
 	}
 
 }
